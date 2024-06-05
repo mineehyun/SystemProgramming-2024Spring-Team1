68f609fd18c1f3184576356535f71f9d0a994139
민우 이 <jj124213@ajou.ac.kr>
2024-06-05 16:11:25 +0900

    chore: minor changes Removed comments Moved init/final message from stderr to stdout

--- gpio.c
+++ gpio.c
@@ -4,7 +4,7 @@
 
 void GPIOExport(int pin)
 {
-    fprintf(stderr, "[GPIOExport] Exporting pin %d\n", pin);
+    printf("[GPIOExport] Exporting pin %d\n", pin);
     int fd = open(GPIO_EXPORT_PATH, O_WRONLY);
     if (fd == -1)
     {
@@ -18,12 +18,12 @@ void GPIOExport(int pin)
         return;
     }
     close(fd);
-    fprintf(stderr, "[GPIOExport] Exported pin %d\n", pin);
+    printf("[GPIOExport] Exported pin %d\n", pin);
 }
 
 void GPIOUnexport(int pin)
 {
-    fprintf(stderr, "[GPIOExport] Exporting pin %d\n", pin);
+    printf("[GPIOExport] Unexporting pin %d\n", pin);
     int fd = open(GPIO_UNEXPORT_PATH, O_WRONLY);
     if (fd == -1)
     {
@@ -37,33 +37,29 @@ void GPIOUnexport(int pin)
         return;
     }
     close(fd);
-    fprintf(stderr, "[GPIOExport] Exported pin %d\n", pin);
+    printf("[GPIOExport] Unexported pin %d\n", pin);
 }
 
 void GPIODirection(int pin, int dir)
 {
-    fprintf(stderr, "[GPIODirection] Directing pin %d\n", pin);
-    /* @dir exception */
+    printf("[GPIODirection] Directing pin %d\n", pin);
     if (dir != IN && dir != OUT)
     {
         fprintf(stderr, "[GPIODirection] @dir must be IN(0) or OUT(1)\n");
         return;
     }
-    /* Set path */
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, GPIO_DIRECTION_PATH, pin) == -1)
     {
         perror("[GPIODirection] Path write failed");
         return;
     }
-    /* Open file */
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
         perror("[GPIODirection] File open failed");
         return;
     }
-    /* Write */
     char *dir_strs[2] = { "in", "out" };
     if (dprintf(fd, dir_strs[dir]) == -1)
     {
@@ -71,72 +67,62 @@ void GPIODirection(int pin, int dir)
         close(fd);
         return;
     }
-    /* Finalize */
     close(fd);
-    fprintf(stderr, "[GPIODirection] Directed pin %d\n", pin);
+    printf("[GPIODirection] Directed pin %d\n", pin);
 }
 
 void GPIOWrite(int pin, int value)
 {
-    fprintf(stderr, "[GPIOWrite] Writing value %d pin %d\n", value, pin);
-    /* @value exception */
+    printf("[GPIOWrite] Writing value %d pin %d\n", value, pin);
     if (value != HIGH && value != LOW)
     {
         fprintf(stderr, "[GPIOWrite] @value must be LOW(0) or HIGH(1)\n");
         return;
     }
-    /* Set path */
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, GPIO_VALUE_PATH, pin) == -1)
     {
         perror("[GPIOWrite] Path write failed");
         return;
     }
-    /* Open file */
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
         perror("[GPIOWrite] File open failed");
         return;
     }
-    /* Write */
     if (dprintf(fd, "%d", value) == -1)
     {
         perror("[GPIOWrite] Write failed");
         close(fd);
         return;
     }
-    /* Finalize */
     close(fd);
-    fprintf(stderr, "[GPIOWrite] Writed value %d pin %d\n", value, pin);
+    printf("[GPIOWrite] Writed pin %d value %d\n", pin, value);
 }
 
 int GPIORead(int pin)
 {
-    fprintf(stderr, "[GPIORead] Reading pin %d\n", pin);
-    /* Set path */
+    printf("[GPIORead] Reading pin %d\n", pin);
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, GPIO_VALUE_PATH, pin) == -1)
     {
         perror("[GPIORead] Path write failed");
         return -1;
     }
-    /* Open file */
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
         perror("[GPIORead] File open failed");
         return -1;
     }
-    /* Read */
     if (read(fd, buffer, 3) == -1)
     {
         perror("[GPIORead] Read failed");
         close(fd);
         return -1;
     }
-    /* Finalize */
     close(fd);
-    fprintf(stderr, "[GPIORead] Read pin %d value %s\n", pin, buffer);
+    printf("[GPIORead] Read pin %d value %s\n", pin, buffer);
     return atoi(buffer);
 }
