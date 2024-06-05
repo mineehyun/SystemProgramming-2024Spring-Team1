ec4aac73fe0a6e2f1ff2553963bf611047f4708e
민우 이 <jj124213@ajou.ac.kr>
2024-06-05 22:21:33 +0900

    style: added error handling `socket` And added some descriptions

--- socket.c
+++ socket.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -5,6 +6,7 @@
 
 int create_socket_server(int port)
 {
+    printf("[Server] Creating socket port %d\n", port);
     int server_sock, client_sock;
     struct sockaddr_in server_address = 
     {
@@ -13,19 +15,40 @@ int create_socket_server(int port)
         .sin_port = port,
     };
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock == -1) return -1;
-    if (bind(server_sock, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) return -1;
-    if (listen(server_sock, 5) == -1) return -1;
-    /* Wait for connection request */
+    if (server_sock == -1)
+    {
+        perror("[Server] Socket failed");
+        return -1;
+    }
+    if (bind(server_sock, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        perror("[Server] Bind failed");
+        close(server_sock);
+        return -1;
+    }
+    if (listen(server_sock, 5) == -1)
+    {
+        perror("[Server] Listen failed");
+        close(server_sock);
+        return -1;
+    }
     struct sockaddr_in client_address;
     socklen_t client_address_len = sizeof(client_address);
     client_sock = accept(server_sock, (struct sockaddr *)&client_address, &client_address_len);
-    if (client_sock == -1) return -1;
+    if (client_sock == -1)
+    {
+        perror("[Server] Accept failed");
+        close(server_sock);
+        return -1;
+    }
+    close(server_sock);
+    printf("[Server] Created socket port %d\n", port);
     return client_sock;
 }
 
 int create_socket_client(char address[], int port)
 {
+    printf("[Client] Creating socket address %s port %d\n", address, port);
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_address = 
     {
@@ -33,6 +56,12 @@ int create_socket_client(char address[], int port)
         .sin_family = AF_INET,
         .sin_port = port,
     };
-    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) return -1;
+    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        perror("[Client] Connect failed");
+        close(sock);
+        return -1;
+    }
+    printf("[Client] Created socket address %s port %d\n", address, port);
     return sock;
 }
--- socket.h
+++ socket.h
@@ -1,31 +1,29 @@
 #ifndef __SOCKET_H__
 #define __SOCKET_H__
 
-#define SOCKET_SOCKET_ERRNO -1
-#define SOCKET_BIND_ERRNO -2
-#define SOCKET_LISTEN_ERRNO -3
-#define SOCKET_ACCEPT_ERRNO -4
-#define SOCKET_CONNECT_ERRNO -5
-
 #define DEFAULT_PORT 8888
 
 /**
  * create_socket_server(@port)
- * Create a socket as server and wait for connection.
- * Socket must be closed by `close`.
+ * DESCRIPTION
+ *  Create a socket as server and wait for connection.
+ *  Socket must be closed by `close`.
+ *  Note that server socket will be closed before function return.
+ *  So cannot use same server socket to accept two or more clients.
  * 
  * RETURN
- * Return the connected socket. Negative means error.
+ *  Return the connected socket. Negative means error.
  */
 int create_socket_server(int port);
 
 /**
  * create_socket_client(@address, @port)
- * Create a socket as client and request connection to @address @port.
- * Socket must be closed by `close`.
+ * DESCRIPTION
+ *  Create a socket as client and request connection to @address @port.
+ *  Socket must be closed by `close`.
  * 
  * RETURN
- * Return the connected socket. Negative means error.
+ *  Return the connected socket. Negative means error.
  */
 int create_socket_client(char address[], int port);
 
