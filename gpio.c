a2bc695ac445883d6c237947aacb87c49c6f0f01
민우 이 <jj124213@ajou.ac.kr>
2024-06-06 12:59:37 +0900

    에러 메시지 출력 수정

--- gpio.c
+++ gpio.c
@@ -4,125 +4,93 @@
 
 void GPIOExport(int pin)
 {
-    printf("[GPIOExport] Exporting pin %d\n", pin);
     int fd = open(GPIO_EXPORT_PATH, O_WRONLY);
     if (fd == -1)
     {
-        perror("[GPIOExport] File open failed");
-        return;
+        perror("[GPIOExport] open: ");
     }
-    if (dprintf(fd, "%d", pin) == -1)
+    else if (dprintf(fd, "%d", pin) == -1)
     {
-        perror("[GPIOExport] Write failed");
-        close(fd);
-        return;
+        perror("[GPIOExport] dprintf: ");
     }
     close(fd);
-    printf("[GPIOExport] Exported pin %d\n", pin);
 }
 
 void GPIOUnexport(int pin)
 {
-    printf("[GPIOUnexport] Unexporting pin %d\n", pin);
     int fd = open(GPIO_UNEXPORT_PATH, O_WRONLY);
     if (fd == -1)
     {
-        perror("[GPIOUnexport] File open failed");
-        return;
+        perror("[GPIOUnexport] open: ");
     }
-    if (dprintf(fd, "%d", pin) == -1)
+    else if (dprintf(fd, "%d", pin) == -1)
     {
-        perror("[GPIOUnexport] Write failed");
-        close(fd);
-        return;
+        perror("[GPIOUnexport] dprintf: ");
     }
     close(fd);
-    printf("[GPIOUnexport] Unexported pin %d\n", pin);
 }
 
 void GPIODirection(int pin, int dir)
 {
-    printf("[GPIODirection] Directing pin %d\n", pin);
-    if (dir != IN && dir != OUT)
-    {
-        fprintf(stderr, "[GPIODirection] @dir must be IN(0) or OUT(1)\n");
-        return;
-    }
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, GPIO_DIRECTION_PATH, pin) == -1)
     {
-        perror("[GPIODirection] Path write failed");
+        perror("[GPIODirection] snprintf: ");
         return;
     }
     int fd = open(buffer, O_WRONLY);
+    char *dir_strs[2] = { "in", "out" };
     if (fd == -1)
     {
-        perror("[GPIODirection] File open failed");
-        return;
+        perror("[GPIODirection] open: ");
     }
-    char *dir_strs[2] = { "in", "out" };
-    if (dprintf(fd, dir_strs[dir]) == -1)
+    else if (dprintf(fd, dir_strs[dir]) == -1)
     {
-        perror("[GPIODirection] Write failed");
-        close(fd);
-        return;
+        perror("[GPIODirection] dprintf: ");
     }
     close(fd);
-    printf("[GPIODirection] Directed pin %d\n", pin);
 }
 
 void GPIOWrite(int pin, int value)
 {
-    printf("[GPIOWrite] Writing value %d pin %d\n", value, pin);
-    if (value != HIGH && value != LOW)
-    {
-        fprintf(stderr, "[GPIOWrite] @value must be LOW(0) or HIGH(1)\n");
-        return;
-    }
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, GPIO_VALUE_PATH, pin) == -1)
     {
-        perror("[GPIOWrite] Path write failed");
+        perror("[GPIOWrite] snprintf: ");
         return;
     }
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
-        perror("[GPIOWrite] File open failed");
-        return;
+        perror("[GPIOWrite] open: ");
     }
-    if (dprintf(fd, "%d", value) == -1)
+    else if (dprintf(fd, "%d", value) == -1)
     {
-        perror("[GPIOWrite] Write failed");
-        close(fd);
-        return;
+        perror("[GPIOWrite] dprintf: ");
     }
     close(fd);
-    printf("[GPIOWrite] Writed pin %d value %d\n", pin, value);
 }
 
 int GPIORead(int pin)
 {
-    printf("[GPIORead] Reading pin %d\n", pin);
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, GPIO_VALUE_PATH, pin) == -1)
     {
-        perror("[GPIORead] Path write failed");
+        perror("[GPIORead] snprintf: ");
         return -1;
     }
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
-        perror("[GPIORead] File open failed");
+        perror("[GPIORead] open: ");
         return -1;
     }
     if (read(fd, buffer, 3) == -1)
     {
-        perror("[GPIORead] Read failed");
+        perror("[GPIORead] read: ");
         close(fd);
         return -1;
     }
     close(fd);
-    printf("[GPIORead] Read pin %d value %s\n", pin, buffer);
     return atoi(buffer);
 }
--- pwm.c
+++ pwm.c
@@ -4,186 +4,119 @@
 
 void PWMExport(int pwm_num)
 {
-    printf("[PWMExport] Exporting pwm %d\n", pwm_num);
-    if (pwm_num != 0 && pwm_num != 1)
-    {
-        fprintf(stderr, "[PWMExport] @pwm_num must be 0 or 1\n");
-        return;
-    }
     int fd = open(PWM_EXPORT_PATH, O_WRONLY);
     if (fd == -1)
     {
-        perror("[PWMExport] File open failed");
-        return;
+        perror("[PWMExport] open: ");
     }
-    if (dprintf(fd, "%d", pwm_num) == -1)
+    else if (dprintf(fd, "%d", pwm_num) == -1)
     {
-        perror("[PWMExport] Write failed");
-        close(fd);
-        return;
+        perror("[PWMExport] dprintf: ");
     }
     close(fd);
-    printf("[PWMExport] Exported pwm %d\n", pwm_num);
 }
 
 void PWMUnexport(int pwm_num)
 {
-    printf("[PWMUnexport] Unexporting pwm %d\n", pwm_num);
-    if (pwm_num != 0 && pwm_num != 1)
-    {
-        fprintf(stderr, "[PWMUnexport] @pwm_num must be 0 or 1\n");
-        return;
-    }
     int fd = open(PWM_UNEXPORT_PATH, O_WRONLY);
     if (fd == -1)
     {
-        perror("[PWMUnexport] File open failed");
-        return;
+        perror("[PWMUnexport] open: ");
     }
-    if (dprintf(fd, "%d", pwm_num) == -1)
+    else if (dprintf(fd, "%d", pwm_num) == -1)
     {
-        perror("[PWMUnexport] Write failed");
-        close(fd);
-        return;
+        perror("[PWMUnexport] dprintf: ");
     }
     close(fd);
-    printf("[PWMUnexport] Unexported pwm %d\n", pwm_num);
 }
 
 void PWMEnable(int pwm_num)
 {
-    printf("[PWMEnable] Enabling pwm %d\n", pwm_num);
-    if (pwm_num != 0 && pwm_num != 1)
-    {
-        fprintf(stderr, "[PWMEnable] @pwm_num must be 0 or 1\n");
-        return;
-    }
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, PWM_ENABLE_PATH, pwm_num) == -1)
     {
-        perror("[PWMEnable] Path write failed");
+        perror("[PWMEnable] snprintf: ");
         return;
     }
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
-        perror("[PWMEnable] File open failed");
-        return;
+        perror("[PWMEnable] open: ");
     }
-    if (dprintf(fd, "1") == -1)
+    else if (dprintf(fd, "1") == -1)
     {
-        perror("[PWMEnable] Write failed");
-        close(fd);
-        return;
+        perror("[PWMEnable] dprintf: ");
     }
     close(fd);
-    printf("[PWMEnable] Enabled pwm %d\n", pwm_num);
 }
 
 void PWMDisable(int pwm_num)
 {
-    printf("[PWMDisable] Disabling pwm %d\n", pwm_num);
-    if (pwm_num != 0 && pwm_num != 1)
-    {
-        fprintf(stderr, "[PWMDisable] @pwm_num must be 0 or 1\n");
-        return;
-    }
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, PWM_ENABLE_PATH, pwm_num) == -1)
     {
-        perror("[PWMDisable] Path write failed");
+        perror("[PWMDisable] snprintf: ");
         return;
     }
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
-        perror("[PWMDisable] File open failed");
-        return;
+        perror("[PWMDisable] open: ");
     }
-    if (dprintf(fd, "0") == -1)
+    else if (dprintf(fd, "0") == -1)
     {
-        perror("[PWMDisable] Write failed");
-        close(fd);
-        return;
+        perror("[PWMDisable] dprintf: ");
     }
     close(fd);
-    printf("[PWMDisable] Disabled pwm %d\n", pwm_num);
 }
 
 void PWMWritePeriod(int pwm_num, int value)
 {
-    printf("[PWMWritePeriod] Writing period %d pwm %d\n", value, pwm_num);
-    if (pwm_num != 0 && pwm_num != 1)
-    {
-        fprintf(stderr, "[PWMWritePeriod] @pwm_num must be 0 or 1\n");
-        return;
-    }
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, PWM_PERIOD_PATH, pwm_num) == -1)
     {
-        perror("[PWMWritePeriod] Path write failed");
+        perror("[PWMPeriod] snprintf: ");
         return;
     }
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
-        perror("[PWMWritePeriod] File open failed");
-        return;
+        perror("[PWMPeriod] open: ");
     }
-    if (dprintf(fd, "%d", value) == -1)
+    else if (dprintf(fd, "%d", value) == -1)
     {
-        perror("[PWMWritePeriod] Write failed");
-        close(fd);
-        return;
+        perror("[PWMPeriod] dprintf: ");
     }
     close(fd);
-    printf("[PWMWritePeriod] Writed period %d pwm %d\n", value, pwm_num);
 }
 
 void PWMWriteDutyCycle(int pwm_num, int value)
 {
-    printf("[PWMWriteDutyCycle] Writing duty cycle %d pwm %d\n", value, pwm_num);
-    if (pwm_num != 0 && pwm_num != 1)
-    {
-        fprintf(stderr, "[PWMWriteDutyCycle] @pwm_num must be 0 or 1\n");
-        return;
-    }
     char buffer[BUFLEN];
     if (snprintf(buffer, BUFLEN, PWM_DUTYCYCLE_PATH, pwm_num) == -1)
     {
-        perror("[PWMWriteDutyCycle] Path write failed");
+        perror("[PWMDutyCycle] snprintf: ");
         return;
     }
     int fd = open(buffer, O_WRONLY);
     if (fd == -1)
     {
-        perror("[PWMWriteDutyCycle] File open failed");
-        return;
+        perror("[PWMDutyCycle] open: ");
     }
-    if (dprintf(fd, "%d", value) == -1)
+    else if (dprintf(fd, "%d", value) == -1)
     {
-        perror("[PWMWriteDutyCycle] Write failed");
-        close(fd);
-        return;
+        perror("[PWMDutyCycle] dprintf: ");
     }
     close(fd);
-    printf("[PWMWriteDutyCycle] Writed duty cycle %d pwm %d\n", value, pwm_num);
 }
 
 void PWMWriteRatio(int pwm_num, float ratio)
 {
-    printf("[PWMWriteRatio] Writing ratio %.2f%% pwm %d\n", ratio * 100, pwm_num);
-    if (pwm_num != 0 && pwm_num != 1)
-    {
-        fprintf(stderr, "[PWMWriteRatio] @pwm_num must be 0 or 1\n");
-        return;
-    }
     if (ratio < 0 || 1 < ratio)
     {
-        fprintf(stderr, "[PWMWriteRatio] @ratio must be between 0 and 1\n");
+        fprintf(stderr, "[PWMRatio] Invalid ratio %.2f\n", ratio);
         return;
     }
     PWMWritePeriod(pwm_num, DEFAULT_PERIOD);
     PWMWriteDutyCycle(pwm_num, (int)(ratio * DEFAULT_PERIOD));
-    printf("[PWMWriteRatio] Writed ratio %.2f%% pwm %d\n", ratio * 100, pwm_num);
 }
