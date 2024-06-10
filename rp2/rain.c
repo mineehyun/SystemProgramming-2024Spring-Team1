f0233771e72e67fd5930befb19d8666e4149e70b
jj124213 <jj124213@ajou.ac.kr>
2024-06-10 18:33:24 +0900

    redirectory

--- rp2/rain.c
+++ rp2/rain.c
@@ -5,17 +5,16 @@
 #include <poll.h>
 #include "gpio1.h"
 
-#define NO_RAIN_GPIO 14
+#define NO_RAIN_GPIO 17
 // flag == 1: no_rain 
 // flag == 0: yes_rain
 
 void* rain_function(void* rain) {
+    int* rainF = (int*)rain;
     GPIOExport(NO_RAIN_GPIO);
     GPIODirection(NO_RAIN_GPIO, 0);
-    int rain_flag = GPIORead(NO_RAIN_GPIO);
-    rain = &rain_flag;
-    printf("%d", rain_flag);
-
+    *rainF = GPIORead(NO_RAIN_GPIO);
+    // printf("%d", rain_flag);
     GPIOUnexport(NO_RAIN_GPIO);
-    return rain;
+    return rainF;
 }
